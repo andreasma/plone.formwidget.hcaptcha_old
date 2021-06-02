db9bd9c3c90fa466f8b14b9851877dc1e58a7edf
andreasma <maand@gmx.de>
2021-06-02 23:20:41 +0200

    add the modules and additions for the hcaptcha validation

---
+++ src/plone/formwidget/hcaptcha/validator.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from plone.formwidget.hcaptcha import _
+from Acquisition import aq_inner
+from z3c.form import validator
+from zope.component import getMultiAdapter
+from zope.schema import ValidationError
+
+
+
+class WrongCaptchaCode(ValidationError):
+    __doc__ = _(u"The code you entered was wrong, please enter the new one.")
+
+
+class HCaptchaValidator(validator.SimpleFieldValidator):
+    def validate(self, value):
+        super(HCaptchaValidator, self).validate(value)
+        captcha = getMultiAdapter(
+            (aq_inner(self.context), self.request), name="hcaptcha"
+        )
+        if not captcha.verify():
+            raise WrongCaptchaCode
+        return True
